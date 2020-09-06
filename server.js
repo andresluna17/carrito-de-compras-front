6bb8761396f5cb076f0069c0eecc1f139d2207b3
Andres Luna <jaime.pibe41@gmail.com>
2020-09-06 13:47:43 -0500

    deploy heroku 2

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.static("./dist/carrito-de-compras-fron"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/acarrito-de-compras-fron/" })
+  res.sendFile(
+    path.join(__dirname + "/dist/acarrito-de-compras-fron/index.html")
+  )
 );
 
 app.listen(process.env.PORT || 8080);
